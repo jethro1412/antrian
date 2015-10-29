d69395225122e3b21c33ef12d612ff8446ab1035
Sony Surahman <sony.surahman98@gmail.com>
2015-10-30 02:00:47 +0700

    default template head and foot

---
+++ application/views/template/header.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Loket Antrian</title>
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta content="width=device-width, initial-scale=1" name="viewport"/>
+	<meta content="" name="description"/>
+	<meta content="" name="author"/>
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url() ?>assets/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url() ?>assets/css/my.css">
+</head>
+<body>
