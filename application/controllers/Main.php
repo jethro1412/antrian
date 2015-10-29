ec3653abc4d7e32df98bf653c861861544cda1ca
Sony Surahman <sony.surahman98@gmail.com>
2015-10-30 02:01:36 +0700

    update controller sama view

--- application/controllers/Main.php
+++ application/controllers/Main.php
@@ -3,7 +3,9 @@
 class Main extends CI_Controller {
 
 	public function index() {
+		$this->load->view('template/header', FALSE);
 		$this->load->view('loket', FALSE);
+		$this->load->view('template/footer', FALSE);
 		// echo 'hahaha';
 	}
 }
