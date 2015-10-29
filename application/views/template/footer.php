649061bd2f47ba0ccf85d8e020883deec9527521
Sony Surahman <sony.surahman98@gmail.com>
2015-10-30 03:33:32 +0700

    ganti dari bootstrap ke googlecss materialize

--- application/views/template/footer.php
+++ application/views/template/footer.php
@@ -1,5 +1,5 @@
 
-<script type="text/javascript" src="<?php echo base_url(); ?>assets/bootstrap.min.js"></script>
-<script type="text/javascript" src="<?php echo base_url(); ?>assets/bootstrap.min.js"></script>
+<script type="text/javascript" src="<?php echo base_url(); ?>dist/js/jquery.js"></script>
+<script type="text/javascript" src="<?php echo base_url(); ?>dist/js/materialize.js"></script>
 </body>
 </html>
